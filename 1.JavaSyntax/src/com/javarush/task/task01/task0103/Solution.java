f1fbceb0da8c9e957026fa05abf544ac95b6fef9
Dmitrii Leliuhin <dleliuhin@mail.ru>
2018-07-08 02:04:22 +0300

    DLeliuhin

--- 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -6,6 +6,8 @@ package com.javarush.task.task01.task0103;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int year=3126;
+        int friend=year-8;
+        System.out.print(friend);
     }
 }
