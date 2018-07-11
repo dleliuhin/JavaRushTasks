4caca46492eea9c7f638f60f27c327d7787794ab
Dmitrii Leliuhin <dleliuhin@mail.ru>
2018-07-11 05:31:38 +0300

    DLeliuhin

--- 1.JavaSyntax/src/com/javarush/task/task02/task0202/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0202/Solution.java
@@ -5,10 +5,13 @@ package com.javarush.task.task02.task0202;
 */
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Person person = new Person();
     }
 
     public static class Person {
-        //напишите тут ваш код
+        String name;
+        int age;
+        int weight;
+        int money;
     }
 }
